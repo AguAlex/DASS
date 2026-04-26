4f4cee9758491fd7e4927ac90eecdef6d0cf3c28
Agusoaei Alexandru <66787826+AguAlex@users.noreply.github.com>
2026-04-26 18:50:10 +0300

    implementare csp

--- dass/next.config.ts
+++ dass/next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Functia headers() va injecta CSP-ul pe absolut toate rutele aplicatiei
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Se aplica pe toate rutele
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
